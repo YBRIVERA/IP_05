e78f5b012351fb3e5c14fb0dca9e888d000e481d
YBRIVERA <142907885+YBRIVERA@users.noreply.github.com>
2023-11-14 21:52:23 -0600

    Add files via upload

---
+++ semana 12/labno12_banelly_rivera/labno12_banelly_rivera/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Circulo
+{
+    private double radio;
+
+    public Circulo(double radio)
+    {
+        this.radio = radio;
+    }
+
+    private double ObtenerPerimetro()
+    {
+        return 2 * Math.PI * radio;
+    }
+
+    private double ObtenerArea()
+    {
+        return Math.PI * Math.Pow(radio, 2);
+    }
+
+    private double ObtenerVolumen()
+    {
+        return (4.0 / 3.0) * Math.PI * Math.Pow(radio, 3);
+    }
+
+    public void CalcularGeometria(ref double unPerimetro, ref double unArea, ref double unVolumen)
+    {
+        unPerimetro = ObtenerPerimetro();
+        unArea = ObtenerArea();
+        unVolumen = ObtenerVolumen();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        double radioCirculo;
+        Console.Write("Ingrese el radio del círculo: ");
+        while (!double.TryParse(Console.ReadLine(), out radioCirculo) || radioCirculo <= 0)
+        {
+            Console.WriteLine("Ingrese un valor el cual sea válido para el radio.");
+        }
+
+        Circulo objCirculo = new Circulo(radioCirculo);
+
+        double perimetro = 0, area = 0, volumen = 0;
+
+        objCirculo.CalcularGeometria(ref perimetro, ref area, ref volumen);
+
+        Console.WriteLine("El perímetro del círculo es: " + perimetro);
+        Console.WriteLine("El área del círculo es: " + area);
+        Console.WriteLine("El volumen del círculo es: " + volumen);
+
+        Console.ReadLine();
+    }
+}
